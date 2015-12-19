78f8b271d170727b663e0175e60192b9e7f5bd2c
Víctor <viic.laspalmas@gmail.com>
2015-12-19 13:08:41 +0000

    Añadiendo angular-google-maps a package.json, añadidas dependencias a index.html

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var app = express();
 
+//views directory
+
 app.set("views", __dirname + "/public");
+
+//public dependencies
 
 app.use(express.static('public'));
 app.use("/scripts/angular", express.static(__dirname + "/node_modules/angular"));
 app.use("/scripts/jquery", express.static(__dirname + "/node_modules/jquery/dist"));
 app.use("/scripts/angular-material", express.static(__dirname + "/node_modules/angular-material"));
+app.use("/scripts/angular-google-maps", express.static(__dirname + "/node_modules/angular-google-maps/dist"));
+app.use("/scripts/lodash", express.static(__dirname + "/node_modules/angular-google-maps/node_modules/lodash"));
+app.use("/scripts/angular-simple-logger", express.static(__dirname + "/node_modules/angular-google-maps/node_modules/angular-simple-logger/dist"));
 
 app.get('/', function (req, res) {
   res.sendFile('index');
